428067c5193f0fff6b0427f91bfde60a15b89325
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-09-14 21:37:08 +0100

    Fixing deployment bug - DB SSL #2

--- db.js
+++ db.js
@@ -3,10 +3,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 const pool = new Pool({
     user: "zuristage2_user",
-    host: "dpg-ck1habvdorps73ej8ni0-a.oregon-postgres.render.com?ssl=true",
+    host: "dpg-ck1habvdorps73ej8ni0-a.oregon-postgres.render.com",
     database: "zuristage2",
     password: "IS2IBnVXszEY8C3SpGuG5snijySEKXrA",
-    port: 5432
+    port: 5432,
+    ssl: true
 });
 
 export default pool;
